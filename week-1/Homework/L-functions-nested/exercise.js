2bf09d6cf5916801fc0b6648a841d4c516563dd9
Shaminderjeet kaur <shaminderjeet92@gmail.com>
2022-02-24 22:28:30 +0100

    message

--- week-1/Homework/L-functions-nested/exercise.js
+++ week-1/Homework/L-functions-nested/exercise.js
@@ -3,3 +3,11 @@ var mentor2 = "Irina";
 var mentor3 = "Mimi";
 var mentor4 = "Rob";
 var mentor5 = "Yohannes";
+function Greeting(greet){
+    return(greet)
+}
+console.log(`HELLO ${mentor1.toUpperCase()}`)
+console.log(`HELLO ${mentor2.toUpperCase()}`)
+console.log(`HELLO ${mentor3.toUpperCase()}`)
+console.log(`HELLO ${mentor4.toUpperCase()}`)
+console.log(`HELLO ${mentor5.toUpperCase()}`)
