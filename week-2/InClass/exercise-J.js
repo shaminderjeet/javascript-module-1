b413684f36b6ff4ddd4e47fa85dd8e1ab7e688da
Shaminderjeet kaur <shaminderjeet92@gmail.com>
2022-03-05 16:26:08 +0100

    inclasss done

--- week-2/InClass/exercise-J.js
+++ week-2/InClass/exercise-J.js
@@ -1,6 +1,8 @@
 function secondMatchesAmy(array) {
-  if ( ) {
+  if (array[1]=="Amy" ) {
     return "Second index matched!";
   }
   return "Second index not matched";
 }
+console.log(secondMatchesAmy(['Bob','Amy','Alice']))
+console.log(secondMatchesAmy(['Bob','Raj','Alice']))
---
+++ week-2/InClass/exercise-K.js
@@ -0,0 +1,4 @@
+const students=["bob","namo","sachin"]
+for(let i =0; i<students.length;i++){
+    console.log("hello",students[i])
+}
